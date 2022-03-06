c25ef1237c24337fe149ac9440dbf2a609cc6d5e
Robeil Aregawi <silukeen1@gmail.com>
2022-03-06 16:02:23 -0600

    solved first interview prepartion question

--- FirstDay.java
+++ FirstDay.java
@@ -1,4 +1,34 @@
 package InterviewPre;
 
+import java.util.Arrays;
+
 public class FirstDay {
+    //sum of two number
+    public int[] twoSum(int[] nums, int target){
+
+        int[] addNum = new int[2];
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < i; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int second = i;
+                    int first = j;
+                    addNum[0] = first;
+                    addNum[1] = second;
+                }
+            }
+        }
+        return addNum;
+    }
+
+    public static void main(String[] args) {
+        FirstDay fd = new FirstDay();
+
+        int[] nums = {2,5,7,11,15};
+        int[] newArr = fd.twoSum(nums,17);
+
+        System.out.println(Arrays.toString(newArr));
+    }
 }
